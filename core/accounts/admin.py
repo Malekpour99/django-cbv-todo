1149333f98e51f9870fd5b4a6230fe579f5c8414
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-03-31 13:15:23 +0330

    accounts admin - User & Profile registration
    Registered User and Profile Model in the admin panel

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import User, Profile
+
 
 # Register your models here.
+
+admin.site.register(Profile)
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_superuser", "is_active", "created_date")
+    list_filter = ("email", "is_superuser", "is_active")
+    fieldsets = (
+        (
+            "Authentication",
+            {
+                "fields": (
+                    "email",
+                    "password",
+                ),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "user_permissions",
+                    "groups",
+                ),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login",),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
