ac1660a8ef7acc99acca1d009051316e65777c1e
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-03-31 13:09:43 +0330

    accounts models - User model
    Created a custom user model for managing users

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    # objects = UserManager()
+
+    def __str__(self):
+        return self.email
