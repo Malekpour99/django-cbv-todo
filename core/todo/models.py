b3d869c369b31a553e8de053a9f931346b766bd9
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-01 11:13:19 +0330

    todo models - Task model
    Created task model for managing each user tasks

--- core/todo/models.py
+++ core/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    owner = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    is_completed = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
