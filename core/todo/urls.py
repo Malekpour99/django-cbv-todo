e7dfa3b519f83e93a538240c28bd6601064251cf
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-02 11:19:12 +0330

    todo URLs - delete-task URL
    Added delete-task URL for deleting tasks using this URL

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, reverse_lazy
 from django.views.generic import RedirectView
-from .views import TaskListView, TaskCreateView
+from .views import TaskListView, TaskCreateView, TaskDeleteView
+
 app_name = "todo"
 
 urlpatterns = [
     path("", RedirectView.as_view(url=reverse_lazy("todo:tasks")), name="home"),
     path("tasks", TaskListView.as_view(), name="tasks"),
     path("task/create/", TaskCreateView.as_view(), name="create-task"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="delete-task"),
 ]
