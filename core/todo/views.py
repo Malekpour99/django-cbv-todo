046a01be4d0864480873b1471d6694eaa9a7c033
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-02 10:30:21 +0330

    todo views - TaskCreateView
    Created TaskCreateView class for creating tasks

--- core/todo/views.py
+++ core/todo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import Task
+
 
 # Create your views here.
+
+
+class TaskCreateView(CreateView):
+    template_name = "todo/task-list.html"
+    model = Task
+    fields = ["title"]
+    success_url = reverse_lazy("todo:tasks")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
