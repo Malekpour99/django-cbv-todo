0d71fd49737edb8e7b0545d83092dba03316bc99
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-02 10:28:54 +0330

    todo admin - Task registration
    Registered Task model in the admin panel

--- core/todo/admin.py
+++ core/todo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["title", "owner", "created_date", "is_completed"]
+    list_filter = ["owner", "is_completed"]
+    search_fields = ["title", "owner"]
+    date_hierarchy = "created_date"
