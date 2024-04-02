d1ea0aab0fe491cf3524b17c389b927b99aa1868
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-02 10:44:03 +0330

    todo views - TaskListView
    Created TaskListView class for showing each users tasks

--- core/todo/views.py
+++ core/todo/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import ListView, CreateView
 from .models import Task
 
 
 # Create your views here.
+class TaskListView(ListView):
+    template_name = "todo/task-list.html"
+    context_object_name = "tasks"
+
+    def get_queryset(self):
+        tasks = Task.objects.filter(owner=self.request.user)
+        return tasks
 
 
 class TaskCreateView(CreateView):
