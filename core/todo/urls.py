30fa5fef5994c7574fc44bc02f1379a2db08750e
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-02 14:53:35 +0330

    todo URLs - task status URL
    Created a URL path for updating the task status

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, reverse_lazy
 from django.views.generic import RedirectView
-from .views import TaskListView, TaskCreateView, TaskDeleteView
+from .views import TaskListView, TaskCreateView, TaskStatusView, TaskDeleteView
 
 app_name = "todo"
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path("", RedirectView.as_view(url=reverse_lazy("todo:tasks")), name="home"),
     path("tasks", TaskListView.as_view(), name="tasks"),
     path("task/create/", TaskCreateView.as_view(), name="create-task"),
+    path("task/<int:pk>/complete/", TaskStatusView.as_view(), name="complete-task"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="delete-task"),
 ]
