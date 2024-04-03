cded5ad9b54f8d8a811b03d37742e052b1a9f3c8
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-03 11:01:56 +0330

    accounts views - RegisterView
    Created register view form class to managing the registration process of new users

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from .forms import CustomUserCreationForm
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from django.contrib.auth import login
 
 # Create your views here.
+
+
+class RegisterView(FormView):
+    """
+    Managing registration of new users
+    """
+    
+    template_name = "registration/register.html"
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("todo:tasks")
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            # login user after successful registration
+            login(self.request, user)
+        return super().form_valid(form)
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(self.success_url)
+        return super().get(request, *args, **kwargs)
