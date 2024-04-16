7e9ba4e8aa5cc18655481bd6c5c76252276d9c0f
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-16 05:40:32 +0330

    core URLs - api-auth REST framework URL
    Added API-auth URL path of the REST framework to the project's core URL paths in order to use REST framework login and logout views

--- core/core/urls.py
+++ core/core/urls.py
@@ -24,6 +24,7 @@ from django.conf import settings
 urlpatterns = [
     path("", include("todo.urls")),
     path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path("accounts/", include("accounts.urls")),
 ]
 
