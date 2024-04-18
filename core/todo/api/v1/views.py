b87cb0c42ced09cb3b73f5914202e18136604e2b
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-18 11:09:09 +0330

    todo API views - Task model viewset
    Created TaskModelViewSet for managing Task model and its objects by permissions, serializers, filtering, searching and ordering fields

---
+++ core/todo/api/v1/views.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets
+from .serializers import TaskSerializer
+from .permissions import IsAuthenticatedOwner
+from todo.models import Task
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+
+class TaskModelViewSet(viewsets.ModelViewSet):
+    """
+    Retrieving task and creating a task \n
+    Retrieving, Updating and Deleting a single task
+    """
+
+    permission_classes = [IsAuthenticatedOwner]
+    serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ["is_completed", "created_date"]
+    search_fields = ["title"]
+    ordering_fields = ["created_date", "is_completed"]
+    
+    def get_queryset(self):
+        return Task.objects.filter(owner__user__id=self.request.user.id)
