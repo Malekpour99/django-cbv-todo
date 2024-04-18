17db6e232d7f4a5352ea8f91a282c6212dd6f7da
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-18 10:28:34 +0330

    todo models - Task representation & relative URL
    Added representation Dunder and relative path URL getter method for this model API serializer

--- core/todo/models.py
+++ core/todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,3 +10,9 @@ class Task(models.Model):
     is_completed = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
+
+    def get_relative_api_url(self):
+        return reverse("todo:api-v1:task-detail", kwargs={"pk": self.pk})
