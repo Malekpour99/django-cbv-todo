c7a5ca53a6a9faa08894655950307a7cad068b07
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-18 16:21:48 +0330

    todo API views - Task model viewset pagination
    Added custom default pagination as the pagination class to the task model viewset CBV

--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .serializers import TaskSerializer
 from .permissions import IsAuthenticatedOwner
+from .paginations import CustomDefaultPagination
 from todo.models import Task
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -15,6 +16,7 @@ class TaskModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOwner]
     serializer_class = TaskSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    pagination_class = CustomDefaultPagination
     filterset_fields = ["is_completed", "created_date"]
     search_fields = ["title"]
     ordering_fields = ["created_date", "is_completed"]
