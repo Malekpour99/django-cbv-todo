f158aa93cd50612bd75ebda78dff6a91a9343880
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-18 10:25:13 +0330

    core URLs - todo app URL path
    Updated todo app URL path to include its name in the URL path and also redirected all the empty path requests to  the todo app

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,10 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
 
 
 urlpatterns = [
-    path("", include("todo.urls")),
+    path("", lambda request: redirect('todo/')),
+    path("todo/", include("todo.urls")),
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path("accounts/", include("accounts.urls")),
