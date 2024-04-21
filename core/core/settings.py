811bceaf64617c00cdf459e93390c26c79bd66da
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-04-21 05:27:21 +0330

    core settings - drf-yasg
    Added drf-yasg to the project core INSTALLED_APPS for documenting project APIs

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,10 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'django_filters',
     'accounts',
     'todo',
+    'rest_framework',
+    'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
