8d85ae1542366d55b685a6be332e9bfea4ce0b78
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-07 22:25:37 +0330

    accounts API URLs - token logout URL
    Created a new URL path for discarding (deleting) user's authentication token

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Registration
     path("registration/", views.UserRegistration.as_view(), name="registration"),
+    # Token
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
+    path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
 ]
