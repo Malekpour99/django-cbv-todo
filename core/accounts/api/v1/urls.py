7b891ea2d6ec2d08b0f02a0acae0cb1aed5affa3
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-07 21:33:31 +0330

    accounts API URLs - token login URL
    Created a new URL path for obtaining the login token based on the provided user authentication info

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("registration/", views.UserRegistration.as_view(), name="registration"),
+    path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
 ]
