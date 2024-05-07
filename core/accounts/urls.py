265e4e7d6f132665666e154128e5bb7a071aabe3
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-07 05:39:56 +0330

    accounts URLs - app name
    Dedicated an app name to the accounts app for avoiding conflicts between our project URL paths

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import RegisterView
 
+app_name = "accounts"
+
 urlpatterns = [
     path("login/", LoginView.as_view(redirect_authenticated_user=True), name="login"),
     path("logout", LogoutView.as_view(), name="logout"),
