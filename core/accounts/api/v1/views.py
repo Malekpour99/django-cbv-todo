3298ea9ce1496b1e4dc323077b7b2590f5ba46b4
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-07 22:24:55 +0330

    accounts API views - Custom Discard Auth Token CBV
    Created custom discard auth token APIview CBV for deleting user's existing authentication token

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,14 +1,17 @@
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from .serializers import RegistrationSerializer, CustomAuthTokenSerializer
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from django.shortcuts import get_object_or_404
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class UserRegistration(GenericAPIView):
-    """Registration for users"""
+    """Register new users"""
 
     serializer_class = RegistrationSerializer
 
@@ -24,6 +27,8 @@ class UserRegistration(GenericAPIView):
 
 
 class CustomObtainAuthToken(ObtainAuthToken):
+    """Generate an authentication token for user"""
+
     serializer_class = CustomAuthTokenSerializer
 
     def post(self, request, *args, **kwargs):
@@ -34,3 +39,18 @@ class CustomObtainAuthToken(ObtainAuthToken):
         user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
         return Response({"token": token.key, "user_id": user.pk, "email": user.email})
+
+
+class CustomDiscardAuthToken(APIView):
+    """Delete user's existing authentication token"""
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            request.user.auth_token.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except ObjectDoesNotExist:
+            return Response(
+                {"detail": "Token does not exist"}, status=status.HTTP_404_NOT_FOUND
+            )
