813f490d4032b804168f5c89d36307a7c5bd4e97
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-07 21:32:07 +0330

    accounts API views - Custom Obtain Auth Token
    Created Custom Obtain Auth Token CBV in order to return user's email and ID beside the generated token in our response

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,8 +1,10 @@
 from rest_framework.response import Response
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, CustomAuthTokenSerializer
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from django.shortcuts import get_object_or_404
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 
 class UserRegistration(GenericAPIView):
@@ -19,3 +21,16 @@ class UserRegistration(GenericAPIView):
         data = {"email": email}
         # Prevention of receiving hashed password in the serializer.data
         return Response(data, status=status.HTTP_201_CREATED)
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    serializer_class = CustomAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key, "user_id": user.pk, "email": user.email})
