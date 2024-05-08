ae9a8c21764a2704fe1e14f5a1f0b4ef3d317edb
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-08 05:13:58 +0330

    accounts API serializers - Change Password Serializer
    Created change password serializer for validating new and old passwords

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -31,6 +31,22 @@ class RegistrationSerializer(serializers.ModelSerializer):
         # return super().create(validated_data)
 
 
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+    new_password1 = serializers.CharField(required=True)
+
+    def validate(self, attrs):
+        if attrs.get("new_password") != attrs.get("new_password1"):
+            raise serializers.ValidationError({"detail": "Passwords do not match!"})
+
+        try:
+            validate_password(attrs.get("new_password"))
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({"new_password": list(e.messages)})
+        return super().validate(attrs)
+
+
 class CustomAuthTokenSerializer(serializers.Serializer):
     email = serializers.CharField(label=_("Email"), write_only=True)
     password = serializers.CharField(
