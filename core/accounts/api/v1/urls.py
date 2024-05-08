1f1b68d2f246edb9763e9cbd17b342bd400a0fb1
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-08 05:16:44 +0330

    accounts API URLs - change password URL
    Created a URL path for changing user's password

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     # Registration
     path("registration/", views.UserRegistration.as_view(), name="registration"),
+    # Password Management
+    path("change-password/", views.ChangePasswordAPIView.as_view(), name="change-password"),
     # Token
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
     path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
