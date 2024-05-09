469735f54e1e0745f99eaea35d0feeb7fe407b62
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-09 10:47:44 +0330

    accounts API URLs - activation resend URL
    Created a URL path for resending the user account activation email

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -3,25 +3,30 @@ from . import views
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
-    # Registration
+    # Registration ---------------------------------------------------
     path("registration/", views.UserRegistration.as_view(), name="registration"),
-    # Password Management
+    # Password ---------------------------------------------------
     path(
         "change-password/",
         views.ChangePasswordAPIView.as_view(),
         name="change-password",
     ),
-    # Token
+    # Token ---------------------------------------------------
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
     path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
-    # JWT
+    # JWT ---------------------------------------------------
     path("jwt/create/", views.CustomTokenObtainPairView.as_view(), name="jwt-create"),
     path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
     path("jwt/verify/", TokenVerifyView.as_view(), name="jwt-verify"),
-    # Activation
+    # Activation ---------------------------------------------------
     path(
         "activation/confirm/<str:token>",
         views.ActivationAPIView.as_view(),
         name="activation",
+    ),
+    path(
+        "activation/resend/",
+        views.ActivationResendAPIView.as_view(),
+        name="activation-resend",
     ),
 ]
