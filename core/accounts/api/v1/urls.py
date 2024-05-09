fd4d616028f512ed90c1a81fe294956891126bf5
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-09 10:20:44 +0330

    accounts API URLs - activation confirm URL
    Created a URL path for confirming the activation of user accounts

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     # Registration
     path("registration/", views.UserRegistration.as_view(), name="registration"),
     # Password Management
-    path("change-password/", views.ChangePasswordAPIView.as_view(), name="change-password"),
+    path(
+        "change-password/",
+        views.ChangePasswordAPIView.as_view(),
+        name="change-password",
+    ),
     # Token
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
     path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
@@ -14,4 +18,10 @@ urlpatterns = [
     path("jwt/create/", views.CustomTokenObtainPairView.as_view(), name="jwt-create"),
     path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
     path("jwt/verify/", TokenVerifyView.as_view(), name="jwt-verify"),
+    # Activation
+    path(
+        "activation/confirm/<str:token>",
+        views.ActivationAPIView.as_view(),
+        name="activation",
+    ),
 ]
