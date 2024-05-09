ebee865bb430711190d6f73143ccc2d4c50302a7
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-09 18:51:45 +0330

    accounts models - profiles.py
    Separated profile model from user model for better management of models in a large scale project

---
+++ core/accounts/models/profiles.py
@@ -0,0 +1,21 @@
+from django.db import models
+from .users import User
+from django.utils.translation import gettext_lazy as _
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="accounts/", null=True, blank=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.email
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
