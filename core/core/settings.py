bfeb323867c865deb277d348dbb479de409386e4
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-10 22:25:18 +0330

    core settings - Simple JWT configuration of token lifetime
    Added simple JWT configuration and increased the token lifetime to 10 minutes

--- core/core/settings.py
+++ core/core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 from django.urls import reverse_lazy
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,3 +167,8 @@ EMAIL_PORT = 25
 EMAIL_USE_TLS = False
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
+
+#Simple JWT Configuration
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+}
