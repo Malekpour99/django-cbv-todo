24005ee4f44caa8ebc8d52f4a9f91693eaa9562d
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-23 13:25:51 +0330

    core - celery beat configuration - deleting completed tasks
    Added celery beat configuration for deleting completed tasks every 10 minutes

--- core/core/celery.py
+++ core/core/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
@@ -15,3 +16,13 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+
+# Celery Beat Configuration
+app.conf.beat_schedule = {
+    "delete-completed-tasks": {
+        # Deleting completed tasks every 10 minutes
+        "task": "todo.tasks.delete_completed_tasks",
+        "schedule": crontab(minute="*/10"),
+    },
+}
