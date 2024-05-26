bd53f957c3352200474a9a21f31978e943ef0243
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-05-26 09:57:08 +0330

    core settings - caching configuration
    Added caching configuration to the project's core settings based on the Docker compose Redis service

--- core/core/settings.py
+++ core/core/settings.py
@@ -185,3 +185,14 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # Celery Configuration
 CELERY_BROKER_URL = "redis://redis:6379/1"
+
+# Caching configuration
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
