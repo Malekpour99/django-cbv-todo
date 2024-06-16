ea47f2446fc90b943113987a3b92bcb86c2b9f25
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-16 08:48:20 +0330

    accounts API - replacing views helper classes
    Replaced all the helper classes and methods in the accounts API views with their relevant utils classes and methods

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,6 +1,17 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+
+from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView, RetrieveUpdateAPIView
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .serializers import (
     RegistrationSerializer,
     CustomAuthTokenSerializer,
@@ -11,28 +22,15 @@ from .serializers import (
     ResetPasswordSerializer,
     ResetPasswordConfirmSerializer,
 )
-from rest_framework import status
-from rest_framework.generics import GenericAPIView, RetrieveUpdateAPIView
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.shortcuts import get_object_or_404
-from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.auth import get_user_model
-from django.contrib.sites.shortcuts import get_current_site
-from mail_templated import EmailMessage
-from ..utils import EmailThread
-import jwt
 from accounts.models import Profile
+from .utils import TokenHandler, EmailSender
 
 
 User = get_user_model()
 
 
 class UserRegistration(GenericAPIView):
-    """Register new users"""
+    """Registering new users"""
 
     serializer_class = RegistrationSerializer
 
@@ -116,62 +114,11 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
 
-class EmailSender:
-    """Sending email to users"""
-
-    @staticmethod
-    def send_activation_email(request, user):
-        """Send activation email to the user"""
-        token = EmailSender.get_tokens_for_user(user)
-        current_site = get_current_site(request)
-        protocol = "https" if request.is_secure() else "http"
-        domain = current_site.domain
-        email_obj = EmailMessage(
-            "email/activation-email.tpl",
-            {"protocol": protocol, "domain": domain, "token": token},
-            "admin@admin.com",
-            to=[user.email],
-        )
-        EmailThread(email_obj).start()
-
-    @staticmethod
-    def send_resetpassword_email(request, user):
-        """Send password reset email to the user"""
-        token = EmailSender.get_tokens_for_user(user)
-        current_site = get_current_site(request)
-        protocol = "https" if request.is_secure() else "http"
-        domain = current_site.domain
-        email_obj = EmailMessage(
-            "email/resetpassword-email.tpl",
-            {"protocol": protocol, "domain": domain, "token": token},
-            "admin@admin.com",
-            to=[user.email],
-        )
-        EmailThread(email_obj).start()
-
-    @staticmethod
-    def get_tokens_for_user(user):
-        """Return an access token (JWT) based on the user"""
-        refresh = RefreshToken.for_user(user)
-        return str(refresh.access_token)
-
-
 class ActivationAPIView(APIView):
     """Decoding JWT authentication token and activating user account"""
 
     def get(self, request, token, *args, **kwargs):
-        try:
-            token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-            user_id = token.get("user_id", None)
-        except jwt.exceptions.ExpiredSignatureError:
-            return Response(
-                {"details": "Token has been expired"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        except jwt.exceptions.InvalidSignatureError:
-            return Response(
-                {"details": "Token is not valid"}, status=status.HTTP_400_BAD_REQUEST
-            )
+        _, user_id = TokenHandler.Validate_jwt_access_token(token)
         user_obj = User.objects.get(pk=user_id)
         if user_obj.is_verified:
             return Response(
@@ -235,20 +182,7 @@ class ResetPasswordValidateAPIView(APIView):
     """Validating JWT token"""
 
     def get(self, request, token, *args, **kwargs):
-        valid_token = False
-        try:
-            jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-        except jwt.exceptions.ExpiredSignatureError:
-            return Response(
-                {"details": "Token has been expired", "valid_token": valid_token},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        except jwt.exceptions.InvalidSignatureError:
-            return Response(
-                {"details": "Token is not valid", "valid_token": valid_token},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        valid_token = True
+        valid_token, _ = TokenHandler.Validate_jwt_access_token(token)
         data = {
             "token": token,
             "valid_token": valid_token,
